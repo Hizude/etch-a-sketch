b45ccdedc81326d0cab82617405bd19ea0234f49
Hizude <k.serkanertas@gmail.com>
2020-07-13 17:37:10 +0200

    improved code, added general layout

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const grid = document.querySelector(".grid")
-let grid_size = 256;
+const container = document.getElementById("container");
+// let promptSize = prompt("Choose a number between 2 and 64");
+let promptSize  = 8;
+gridSize = promptSize * promptSize;
+const str_fr = "1fr ";
+let temp_columns = str_fr.repeat(promptSize);
 
-let i = 0;
-while (i < grid_size) {
-    let tile = document.createElement("div");
-    tile.classList.add("tile_default");
-    grid.appendChild(tile);
-    i += 1;
+makeGrid();
+hoverBlack();
+
+function makeGrid(){
+    for (let i = 0; i < gridSize; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+    }
+    container.style.gridTemplateColumns = temp_columns;
 }
 
-// fix grid layout
+function hoverBlack(){
+    document.querySelectorAll(".cell").forEach(item => {
+        item.addEventListener("mouseover", event => {
+            item.classList.add("cell_black");
+        })
+    })
+}
+
