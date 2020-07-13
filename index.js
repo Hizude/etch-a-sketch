4c8e8138cf708bf54f23cfba2d2bcb5ad9ea0359
Hizude <k.serkanertas@gmail.com>
2020-07-13 19:46:57 +0200

    project completed. added black, color pen, clear btn and new grid btn functionality

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const container = document.getElementById("container");
-// let promptSize = prompt("Choose a number between 2 and 64");
-let promptSize  = 8;
-gridSize = promptSize * promptSize;
-const str_fr = "1fr ";
-let temp_columns = str_fr.repeat(promptSize);
+let blackPen = document.getElementById("black_btn");
+let colorPen = document.getElementById("color_btn");
+let newGrid = document.getElementById("new_btn");
+let clearGrid = document.getElementById("clear_btn");
 
 makeGrid();
-hoverBlack();
 
 function makeGrid(){
+    let promptSize = prompt("Number of squares per side (2-64 recommended!)");
+    gridSize = promptSize * promptSize;
+    const str_fr = "1fr ";
+    let temp_columns = str_fr.repeat(promptSize);
     for (let i = 0; i < gridSize; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
@@ -20,8 +22,37 @@ function makeGrid(){
 function hoverBlack(){
     document.querySelectorAll(".cell").forEach(item => {
         item.addEventListener("mouseover", event => {
-            item.classList.add("cell_black");
+            item.style.backgroundColor = "black";
         })
     })
 }
 
+function hoverColor(){
+    document.querySelectorAll(".cell").forEach(item => {
+        item.addEventListener("mouseover", event => {
+            let randColor = "#"+((1<<24)*Math.random()|0).toString(16);
+            item.style.backgroundColor = randColor;
+        })
+    })
+}
+
+newGrid.addEventListener("click", () => {
+    document.querySelectorAll(".cell").forEach(item => {
+        container.removeChild(item);
+    })
+    makeGrid();
+})
+
+clearGrid.addEventListener("click", () => {
+    document.querySelectorAll(".cell").forEach(item => {
+        item.style.backgroundColor = "#f5f5f5";
+    })
+})
+
+blackPen.addEventListener("click", () => {
+    hoverBlack();
+})
+
+colorPen.addEventListener("click", () => {
+    hoverColor();
+})
